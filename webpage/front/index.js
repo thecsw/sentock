06cf8fc70052671fa8e2efc92f74e2d4fe56117b
= <=>
2020-04-03 07:41:56 +0000

    Add very basic chart

--- webpage/front/index.js
+++ webpage/front/index.js
@@ -4,3 +4,23 @@ let app = new Vue({
         message: 'blah blah'
     }
 })
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: [{
+        x: 10,
+        y: 20
+    }, {
+        x: 15,
+        y: 10
+    }],
+    options: {
+        scales: {
+            yAxes: [{
+                stacked: true
+            }]
+        }
+    }
+});
