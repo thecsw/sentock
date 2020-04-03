9dadd1ee47d8abf58d3cedcbaca6798972515378
Ross <rhcopeland101@gmail.com>
2020-04-03 14:19:32 -0500

    Api can now receive post request from webpage

--- flask_api/src/app.py
+++ flask_api/src/app.py
@@ -1,11 +1,22 @@
 # flask_web/app.py
-from flask import Flask
+from flask import Flask, request, jsonify
+import sys
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
+#this is where the webpage is going to pull from. It's going 
+#to always pull from here for data. I can't think of anything
+#else that the webpage is going to need for mvp
+
+#webpage sends {"stock_name":"<stockname>"}
+@app.route('/', methods=["POST"])
 def hello_world():
-    return 'Hi'
+    raw_data = request.data
+    stock_name = json.loads(raw_data)['stock_name']
+    print(stock_name, file=sys.stderr)
+
+    return "hello there"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
