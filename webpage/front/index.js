c9e533cac3289c97ffae8218155fa06dd001f0b8
= <=>
2020-04-03 18:59:18 +0000

    Added Graphs inside of cards the manual way

--- webpage/front/index.js
+++ webpage/front/index.js
@@ -1,26 +1,104 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
+})
+
 let app = new Vue({
     el: '#app',
     data: {
-        message: 'blah blah'
+
     }
 })
 
-var ctx = document.getElementById('myChart').getContext('2d');
+let examplePreviewCTX = document.getElementById('example-preview').getContext('2d');
+let exampleFullCTX = document.getElementById('example-full').getContext('2d');
 
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: [{
-        x: 10,
-        y: 20
-    }, {
-        x: 15,
-        y: 10
-    }],
+var myChart = new Chart(examplePreviewCTX, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Favorite Color',
+            data: [
+                Math.random(),
+                Math.random(),
+                Math.random(),
+                Math.random(),
+                Math.random(),
+                Math.random()
+            ],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.4)',
+                'rgba(54, 162, 235, 0.4)',
+                'rgba(255, 206, 86, 0.4)',
+                'rgba(75, 192, 192, 0.4)',
+                'rgba(153, 102, 255, 0.4)',
+                'rgba(255, 159, 64, 0.4)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+
+        }]
+    },
     options: {
         scales: {
             yAxes: [{
-                stacked: true
+                ticks: {
+                    beginAtZero: true
+                }
             }]
         }
     }
-});
+})
+
+var myChart = new Chart(exampleFullCTX, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Favorite Color',
+            data: [
+                Math.random(),
+                Math.random(),
+                Math.random(),
+                Math.random(),
+                Math.random(),
+                Math.random()
+            ],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.4)',
+                'rgba(54, 162, 235, 0.4)',
+                'rgba(255, 206, 86, 0.4)',
+                'rgba(75, 192, 192, 0.4)',
+                'rgba(153, 102, 255, 0.4)',
+                'rgba(255, 159, 64, 0.4)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
