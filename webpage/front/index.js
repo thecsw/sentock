f77503daf7e6b300036917fc874784d4f4440ffd
Dylan Polson <dylanpolson@ku.edu>
2020-04-04 08:41:37 +0000

    Clean up

--- webpage/front/index.js
+++ webpage/front/index.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {});
+    M.Modal.init(document.querySelectorAll('.modal'), {})
 })
 
 let app = new Vue({
@@ -9,7 +8,6 @@ let app = new Vue({
 
     }
 })
-
 
 //generate random color
 let color1 = {
@@ -80,93 +78,3 @@ let exampleChart = {
 }
 let examplePreview = new Chart(document.getElementById('example-preview').getContext('2d'), exampleChart)
 let exampleFull = new Chart(document.getElementById('example-full').getContext('2d'), exampleChart)
-
-// var myChart = new Chart(examplePreviewCTX, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: 'Favorite Color',
-//             data: [
-//                 Math.random(),
-//                 Math.random(),
-//                 Math.random(),
-//                 Math.random(),
-//                 Math.random(),
-//                 Math.random()
-//             ],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.4)',
-//                 'rgba(54, 162, 235, 0.4)',
-//                 'rgba(255, 206, 86, 0.4)',
-//                 'rgba(75, 192, 192, 0.4)',
-//                 'rgba(153, 102, 255, 0.4)',
-//                 'rgba(255, 159, 64, 0.4)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// })
-
-// var myChart = new Chart(exampleFullCTX, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: 'Favorite Color',
-//             data: [
-//                 Math.random(),
-//                 Math.random(),
-//                 Math.random(),
-//                 Math.random(),
-//                 Math.random(),
-//                 Math.random()
-//             ],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.4)',
-//                 'rgba(54, 162, 235, 0.4)',
-//                 'rgba(255, 206, 86, 0.4)',
-//                 'rgba(75, 192, 192, 0.4)',
-//                 'rgba(153, 102, 255, 0.4)',
-//                 'rgba(255, 159, 64, 0.4)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// })
