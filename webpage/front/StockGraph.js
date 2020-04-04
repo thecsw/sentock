d2e3bfc867b15502e712982b2a81c1c6d9db8c4c
Dylan Polson <dylanpolson@ku.edu>
2020-04-04 09:34:12 +0000

    Add object orientation for each graph

--- webpage/front/StockGraph.js
+++ webpage/front/StockGraph.js
@@ -1,13 +1,49 @@
 export class StockGraph {
 
-    constructor(name, stockData, PositivityData) {
+    html
+    id
+    name
+    positivityData
+    stockData
 
+    constructor(name, id, stockData, positivityData) {
+        this.id = id
+        this.name = name
+        this.positivityData = positivityData
+        this.stockData = stockData
+
+        //generate HTML
+        this.html = `
+        <div class="col s12 m6 xl4">
+            <div class="hoverable card grey darken-4">
+                <a class="modal-trigger white-text" href="#${id}-modal">
+                    <div class="card-image">
+                        <canvas id="${id}-preview"></canvas>
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">${name}</span>
+                    </div>
+                </a>
+            </div>
+            <div id="${id}-modal" class="modal grey darken-4">
+                <div class="modal-content white-text">
+                    <h4>${name}</h4>
+                    <canvas id="${id}-full"></canvas>
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    draw() {
+        //generate colors
         const color = {
             r: Math.random()*100+50,
             g: Math.random()*100+50,
             b: Math.random()*100+50
         }
 
+        //construct chart data
         const chart = {
             type: 'line',
             data: {
@@ -15,14 +51,14 @@ export class StockGraph {
                 datasets: [
                     {
                         label: 'Stock Price',
-                        data: stockData,
+                        data: this.stockData,
                         fill: false,
                         borderColor: `rgb(${color.r}, ${color.g}, ${color.b})`,
                         lineTension: 0.2
                     },
                     {
                         label: 'Positivity Rating',
-                        data: PositivityData,
+                        data: this.positivityData,
                         fill: false,
                         borderColor: `rgb(${color.r+100}, ${color.g+100}, ${color.b+100})`,
                         lineTension: 0.2
@@ -31,8 +67,9 @@ export class StockGraph {
             },
             options: {}
         }
-        new Chart(document.getElementById(`${name}-preview`), chart)
-        new Chart(document.getElementById(`${name}-full`), chart)
 
+        //draw on canvas
+        new Chart(document.getElementById(`${this.id}-preview`), chart)
+        new Chart(document.getElementById(`${this.id}-full`), chart)
     }
 }
