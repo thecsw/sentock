8dbdfe835121eb5102e8b740777aa48a1f16bfa0
Dylan Polson <dylanpolson@ku.edu>
2020-04-04 08:59:20 +0000

    Made graph color scheme match

--- webpage/front/StockGraph.js
+++ webpage/front/StockGraph.js
@@ -2,32 +2,10 @@ export class StockGraph {
 
     constructor(name, stockData, PositivityData) {
 
-        //generate random color
-        let color1 = {
-            r: Math.random()*255,
-            g: Math.random()*255,
-            b: Math.random()*255
-        }
-
-        //make sure luminance is high to see
-        while(0.2126*color1.r + 0.7152*color1.g + 0.0722*color1.b < 64) {
-            if(0.2126*color1.r < 0.7152*color1.g && 0.2126*color1.r < 0.0722*color1.b) color1.r++
-            if(0.7152*color1.g < 0.2126*color1.r && 0.7152*color1.g < 0.0722*color1.b) color1.g++
-            if(0.0722*color1.b < 0.7152*color1.g && 0.0722*color1.b < 0.2126*color1.r) color1.b++
-        }
-
-        //generate random color
-        let color2 = {
-            r: Math.random()*255,
-            g: Math.random()*255,
-            b: Math.random()*255
-        }
-
-        //make sure luminance is high to see
-        while(0.2126*color2.r + 0.7152*color2.g + 0.0722*color2.b < 64) {
-            if(0.2126*color2.r < 0.7152*color2.g && 0.2126*color2.r < 0.0722*color2.b) color2.r++
-            if(0.7152*color2.g < 0.2126*color2.r && 0.7152*color2.g < 0.0722*color2.b) color2.g++
-            if(0.0722*color2.b < 0.7152*color2.g && 0.0722*color2.b < 0.2126*color2.r) color2.b++
+        const color = {
+            r: Math.random()*100+50,
+            g: Math.random()*100+50,
+            b: Math.random()*100+50
         }
 
         const chart = {
@@ -39,14 +17,14 @@ export class StockGraph {
                         label: 'Stock Price',
                         data: stockData,
                         fill: false,
-                        borderColor: `rgb(${color1.r}, ${color1.g}, ${color1.b})`,
+                        borderColor: `rgb(${color.r}, ${color.g}, ${color.b})`,
                         lineTension: 0.2
                     },
                     {
                         label: 'Positivity Rating',
                         data: PositivityData,
                         fill: false,
-                        borderColor: `rgb(${color2.r}, ${color2.g}, ${color2.b})`,
+                        borderColor: `rgb(${color.r+100}, ${color.g+100}, ${color.b+100})`,
                         lineTension: 0.2
                     }
                 ]
