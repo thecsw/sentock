d5e4dafa2e66df77e0a84afea2afe2774b22d303
Ross <rhcopeland101@gmail.com>
2020-04-04 17:30:01 -0500

    got the html to load for docs, but not the css

--- flask_api/src/app.py
+++ flask_api/src/app.py
@@ -1,5 +1,5 @@
 # flask_web/app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import sys
 import json
 
@@ -10,13 +10,19 @@ app = Flask(__name__)
 #else that the webpage is going to need for mvp
 
 #webpage sends {"stock_name":"<stockname>"}
-@app.route('/api/', methods=["GET"])
+@app.route('/api/', methods=["POST"])
 def hello_world():
     raw_data = request.data
     stock_name = json.loads(raw_data)['stock_name']
     print(stock_name, file=sys.stderr)
 
     return "hello there"
+
+
+@app.route('/api/docs', methods=["GET"])
+def serve():
+    return render_template('app.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
