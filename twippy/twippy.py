f00bc0a2ded81e10010d082044d2f493d6b1d928
Sandy <thecsw@ku.edu>
2020-04-08 21:09:03 -0500

    better than ever
    Signed-off-by: Sandy <thecsw@ku.edu>

--- twippy/twippy.py
+++ twippy/twippy.py
@@ -1,6 +1,8 @@
+#!/bin/python
 import tweepy
 import json
 import time
+import databa
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 #test out tweet streaming:
@@ -19,34 +21,71 @@ analyzer = SentimentIntensityAnalyzer()
 
 texts = []
 
+# Create the main table
+databa.create_table()
+
+def got_tweet(tweet_id, text, created_at):
+    sentimentValue = float(analyzer.polarity_scores(text)["compound"])
+    if (sentimentValue == 0):
+        return
+    print(f"{text} | sentiment: {sentimentValue}")
+    databa.add_sentiment(
+        "McDonalds", 
+        tweet_id, 
+        text, 
+        created_at, 
+        sentimentValue)
+
 class TestStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        createdAt = time.strptime(status.created_at.replace("+0000", ""))
-        print("New tweet found, extended: " + str(status.truncated) + 
-              ", Retweet: " + str(hasattr(status, 'retweeted_status'))+
-              ", Created At " + str(createdAt))
-        # The tweet is a retweet
-        if hasattr(status, 'retweeted_status'):
-            if status.retweeted_status['truncated']:
-                texts.append((status.retweeted_status['extended_tweet']['full_text'], createdAt))
+        # Get the actual payload
+        msg = status._json
+
+        # make sure it is not a retweet (we ignore retweets)
+        if 'retweeted_status' in msg:
             return True
-        # The tweet text is collapsed because of its length
-        if status.truncated:
-            #print(status.extended_tweet['full_text'])
-            texts.append((status.extended_tweet['full_text'], createdAt))
+
+        # Log the tweet's creation time in unix timestamp
+        createdAt = int(time.mktime(time.strptime(msg["created_at"].replace("+0000",""))))
+        print("New tweet found, extended: " + str((msg['truncated'])) + 
+              ", Retweet: " + str(('retweeted_status' in msg)) + 
+              ", Created At (UNIX): " + str(createdAt))
+        ### Check if the tweet is a retweet:
+        #if 'retweeted_status' in msg:
+        #    # we ignore retweets:
+        #    return True
+            ## Check if retweet is truncated:
+            #if msg['retweeted_status']['truncated']:
+            #    got_tweet(msg['id_str'], msg['retweeted_status']['extended_tweet']['full_text'], createdAt)
+            #    #print(msg['retweeted_status']['extended_tweet']['full_text'])
+            #    #texts.append((msg['retweeted_status']['extended_tweet']['full_text'], createdAt))
+            #    return True
+            ## if the tweet is not truncated:
+            #got_tweet(msg['id_str'], msg['retweeted_status']['text'], createdAt)
+            ##print(msg['retweeted_status']['text'])
+            ##texts.append((msg['retweeted_status']['text'], createdAt))
+            #return True
+        # If the tweet text is collapsed because its too long:
+        if msg['truncated']:
+            got_tweet(msg['id_str'], msg['extended_tweet']['full_text'], createdAt)
+            #print(msg['extended_tweet']['full_text'])
+            #texts.append((msg['extended_tweet']['full_text'], createdAt))
             return True
         # The tweet is a regular one (not a retweet or more than 140? character long)
-        print(status.text)
-        texts.append((status.text, createdAt))
+        got_tweet(msg['id_str'], msg['text'], createdAt)
+        #print(msg['text'])
+        #texts.append((msg['text'], createdAt))
         return True
-
+ 
     # Callback on error
     def on_error(self, status_code):
-        print("Error occured while listening: " + status_code)
         if status_code == 420:
             #returning False in on_error disconnects the stream
-            return False
+            print("Error code 420 occurred, waiting 15 seconds before continuing")
+            time.sleep(15)
+            return True
+        print("Uncaught Error occured while listening: " + str(status_code))
         return False
 
 print("Setting up listener...")
@@ -54,10 +93,8 @@ testStreamListener = TestStreamListener()
 mstream = tweepy.Stream(auth = api.auth, listener=testStreamListener)
 mstream.filter(track=['McDonald,McDonalds,McDonald\'s'], is_async=True)
 print("waiting...")
-time.sleep(30)
+time.sleep(120)
 print("Disconnecting..")
 mstream.disconnect()
-print("Number of texts gathered: " + str(len(texts)))
-print("Analyzing sentiment...")
-for text in texts:
-    print(str(analyzer.polarity_scores(text[0])) + " : " + text[0])
+
+databa.close()
