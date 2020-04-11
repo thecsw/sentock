77c0376a800c718942158efa1eb7a3622e480953
drmk3 <draco.entom45@gmail.com>
2020-04-10 21:34:40 -0500

    undoing frontend 3

--- webpage/front/index.js
+++ webpage/front/index.js
@@ -2,4 +2,4 @@ document.addEventListener("DOMContentLoaded",() => {
     let mygraph = document.querySelector("#mygraph");
     let req = fetch("http://167.172.114.123:5050/api/graphs?h=12");
     req.then((data) => data.blob()).then(function (image) { mygraph.data = image; });
-}
+});
