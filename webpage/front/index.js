3e3408cb44cd1fd8b2dd8d572a17b129d250a6d5
Dylan Polson <dylanpolson@ku.edu>
2020-04-18 23:37:06 +0000

    Change stock names

--- webpage/front/index.js
+++ webpage/front/index.js
@@ -21,11 +21,11 @@ new Vue({
     },
     data: {
         graphs: [
-            new StockGraph('McDonalds', 'MCD', 'VF5PZ9DBDNDKYYSN')
-            // new StockGraph('Chipotle', 'CMG', 'ZDDTI9CUZ6SEF5IZ'),
-            // new StockGraph('Microsoft', 'MSFT', '9O7U8ZRHOF4WU0FN'),
-            // new StockGraph('Disney', 'DIS', 'N0U4CZ8XN5YCEK9U'),
-            // new StockGraph('FedEx', 'FDX', 'YTU231UOF5L0T7WB')
+            new StockGraph('McDonalds', 'MCD', 'VF5PZ9DBDNDKYYSN'),
+            new StockGraph('Google', 'GOOG', 'ZDDTI9CUZ6SEF5IZ'),
+            new StockGraph('Microsoft', 'MSFT', '9O7U8ZRHOF4WU0FN'),
+            new StockGraph('Disney', 'DIS', 'N0U4CZ8XN5YCEK9U'),
+            new StockGraph('Fedex', 'FDX', 'YTU231UOF5L0T7WB')
         ]
     }
 })
