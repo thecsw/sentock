15e2ca6959761d9e539d2d060204398a08a016cf
Jonathan Downs <jmdowns00@gmail.com>
2020-04-19 20:02:30 -0500

    Added testing for sentiments and company analysis

--- twippy/src/twippy.py
+++ twippy/src/twippy.py
@@ -2,7 +2,7 @@
 import tweepy
 import json
 import time
-import databa
+#import databa
 import requests
 import json
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -27,45 +27,45 @@ URL = 'http://server:10000/sentiments'
 session = requests.session()
 
 # List of companies we would like to find in our tweets
-companies = ["McDonalds", "McDonald\'s", 
-            "Chipotle", "Chipotle\'s", 
-            "Microsoft", "Microsoft\'s",
-            "Fedex", "Fedex\'s", 
-            "Disney", "Walt Disney", "Disney\'s",
-            "Tesla", "TSLA", "Tesla\'s",
-            "Google", "Google\'s", "\"Alphabet Inc\"", "\"Alphabet Inc.\"",
-            "Facebook", "Facebook\'s",
-            "Amazon", "Amozon\'s"]
+companies = ["McDonalds", "McDonald\'s",
+             "Chipotle", "Chipotle\'s",
+             "Microsoft", "Microsoft\'s",
+             "Fedex", "Fedex\'s",
+             "Disney", "Disney\'s",
+             "Tesla", "TSLA", "Tesla\'s",
+             "Google", "\"Alphabet Inc\"", "Google\'s",
+             "Facebook", "Facebook\'s",
+             "Amazon", "Amazon\'s"]
 
 # Create the main table
-databa.create_table()
+#databa.create_table()
 
 def got_tweet(tweet_id, text, created_at):
     sentimentValue = float(analyzer.polarity_scores(text)["compound"])
     if (sentimentValue == 0):
-        return
+        return "No sentiment value"
     # Compute the company
     company = ""
-    if companies[0].lower() in text.lower() or companies[1].lower() in text.lower():
+    if companies[0].lower() in text.lower():
         company = "McDonalds"
-    elif companies[2].lower() in text.lower() or companies[3].lower() in text.lower():
+    elif companies[2].lower() in text.lower():
         company = "Chipotle"
-    elif companies[4].lower() in text.lower() or companies[5].lower() in text.lower():
+    elif companies[4].lower() in text.lower():
         company = "Microsoft"
-    elif companies[6].lower() in text.lower() or companies[7].lower() in text.lower():
+    elif companies[6].lower() in text.lower():
         company = "Fedex"
-    elif companies[8].lower() in text.lower() or companies[9].lower() in text.lower() or companies[10].lower() in text.lower():
+    elif companies[8].lower() in text.lower():
         company = "Disney"
-    elif companies[11].lower() in text.lower() or companies[12].lower() in text.lower() or companies[13].lower() in text.lower():
+    elif companies[10].lower() in text.lower() or companies[11].lower() in text.lower():
         company = "Tesla"
-    elif companies[14].lower() in text.lower() or companies[15].lower() in text.lower() or companies[16].lower() in text.lower() or companies[17].lower() in text.lower():
+    elif companies[13].lower() in text.lower() or companies[14].lower() in text.lower():
         company = "Google"
-    elif companies[18].lower() in text.lower() or companies[19].lower() in text.lower():
+    elif companies[16].lower() in text.lower():
         company = "Facebook"
-    elif companies[20].lower() in text.lower() or companies[21].lower() in text.lower():
+    elif companies[18].lower() in text.lower():
         company = "Amazon"
     if company == "":
-        return
+        return "No company"
     print(f"{text} | sentiment: {sentimentValue}", flush=True)
     payload = {
         "company":company,
@@ -74,7 +74,8 @@ def got_tweet(tweet_id, text, created_at):
         "unix":int(created_at)
     }
     # Make the call
-    requests.post(URL, data=json.dumps(payload))
+    #requests.post(URL, data=json.dumps(payload))
+    return company
 
 class SentStreamListener(tweepy.StreamListener):
 
